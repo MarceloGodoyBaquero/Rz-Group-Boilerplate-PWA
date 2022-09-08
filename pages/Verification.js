67f44d21dbc15fe23375427346ae2247735f499c
marcelogodoybaquero <marcelo.godoy@mi.unc.edu.ar>
2022-09-08 14:51:00 -0300

    Linting fix

--- pages/Verification.js
+++ pages/Verification.js
@@ -3,8 +3,14 @@ import MobileLayout from '../components/MobileLayout'
 import Nav from '../components/Nav'
 import Image from 'next/image'
 import LoginImage from '../public/Login.svg'
+import OtpInput from 'react-otp-input'
 
 const Verification = () => {
+  const [otp, setOtp] = React.useState('')
+  const handleChange = (otp) => {
+    setOtp(otp)
+    return console.log(otp)
+  }
   return (
     <MobileLayout>
       <div className={'md:shadow-2xl bg-[#F7F8FA] h-screen flex items-center flex-col'}>
@@ -16,9 +22,15 @@ const Verification = () => {
             <h2>An authentication code has been sent to
               your mail</h2>
           </div>
-          <div className={'w-full m-2'}>
-            <input placeholder={'Email'}
-                   className={'indent-5 outline-0 w-full rounded-[25px] h-[50px] font-bold text-black bg-[#F4F5F7]'}/>
+          <div className={'w-full'}>
+            <OtpInput
+              value={otp}
+              onChange={handleChange}
+              numInputs={4}
+              isInputNum={true}
+              containerStyle={'w-[400px] border-2 border-[#F7F8FA] rounded-xl h-[100px] flex flex-row items-center justify-evenly'}
+              inputStyle={{ backgroundColor: '#F7F8FA', width: '50px', height: '50px', borderRadius: '10px', border: 'none', fontSize: '20px', textAlign: 'center' }}
+            />
           </div>
           <div className={'m-2 w-full h-fit flex flex-col items-center justify-center'}>
             <h2>I didn't receive code. <span className={'text-blue-500 cursor-pointer'}>Resend Code</span></h2>
