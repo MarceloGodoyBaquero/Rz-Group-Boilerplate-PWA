b1bb08ce321b83bd9bba86c08387e7606428b146
marcelogodoybaquero <marcelo.godoy@mi.unc.edu.ar>
2022-09-12 15:51:14 -0300

    fix imports

--- pages/Main.js
+++ pages/Main.js
@@ -4,7 +4,6 @@ import React from 'react'
 
 import MobileLayout from '../components/MobileLayout'
 import Nav from '../components/Nav'
-import Image from 'next/image'
 import { BanknotesIcon, CalendarIcon, ClockIcon } from '@heroicons/react/24/outline'
 
 export default function Main () {
@@ -16,17 +15,51 @@ export default function Main () {
     <MobileLayout>
       <div className={'md:shadow-2xl bg-[#F7F8FA] h-screen flex items-center flex-col'}>
         <Nav location={'Home'}/>
-        <div className={'h-1/2 flex flex-col justify-center'}>
+        <div className={'h-1/2 flex flex-col justify-center w-full'}>
           <div>
             <h2 className={'text-black font-bold text-2xl m-5'}>Take a Ride</h2>
-            <div className={''}></div>
+            <div className={'flex w-full'}>
+              <button className={'font-bold text-white bg-[#5b211f] w-1/2 rounded-3xl h-[200px] m-5'}>RIDE</button>
+              <button className={'bg-gray-500 w-1/2 rounded-3xl h-[200px] m-5'}>RESERVE</button>
+            </div>
           </div>
           <div>
-            <h2 className={'text-black font-bold text-2xl m-5'}>Your actives Rides</h2>
+            <h2 className={'text-black font-bold text-2xl m-5'}>Your actives rides</h2>
+            <div>
+              {viajes.slice(0, 1).map((viaje) => (
+                <div key={viaje} className={'flex w-full'}>
+                  <div className={'bg-white w-full m-3 rounded p-3 flex'}>
+                    <div>
+                      <div className={'bg-gray-500 w-[66px] h-[66px] rounded-[50%] mr-3'}></div>
+                    </div>
+                    <div className={'w-full flex flex-col justify-center'}>
+                      <div className={'flex items-center justify-between'}>
+                        <h3 className={'font-bold'}>Jhon Smith</h3>
+                        <h3 className={'bg-[orange] rounded-2xl text-white pl-2 pr-2'}>Waiting</h3>
+                      </div>
+                      <div className={'flex items-center justify-evenly'}>
+                        <div className={'flex items-center w-full'}>
+                          <CalendarIcon className={'w-[16px]'}/>
+                          {viaje === 1 ? <h3>Today</h3> : <h3>20 Dec, 2020</h3>}
+                        </div>
+                        <div className={'flex items-center w-full'}>
+                          <ClockIcon className={'w-[16px]'}/>
+                          <h3>2 Hours</h3>
+                        </div>
+                        <div className={'flex items-center w-full '}>
+                          <BanknotesIcon className={'w-[16px]'}/>
+                          <h3>$200.00</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         <div
-          className={'bg-[#5b211f] w-full rounded-t-3xl mt-5 p-5 h-1/2 flex-grow flex-col items-center justify-center'}>
+          className={'overflow-auto bg-[#5b211f] w-full rounded-t-3xl mt-5 p-5 h-1/2 flex-grow flex-col items-center justify-center'}>
           <div>
             <h2 className={'text-white font-bold text-2xl text-center m-5'}>Your last trips</h2>
           </div>
