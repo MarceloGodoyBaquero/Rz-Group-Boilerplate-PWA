ea72acf9a49d795a203c59347b99e95854cd9401
tiago cornalo <cornalotiago@gmail.com>
2022-09-13 12:33:54 -0300

    upgrade again react18

--- pages/Fuec.js
+++ pages/Fuec.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FuecTemplate from '../components/FuecTemplate/FuecTemplate'
 import { PDFViewer } from '@react-pdf/renderer'
 export default function Fuec () {
+  //
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
   return (
     <div className='flex content-center items-center justify-center w-[100%] h-[100vh]' >
       {
-        process.browser && <PDFViewer style={{ width: '100%', height: '100vh' }} suppressHydrationWarning={true}>
+        mounted && <PDFViewer style={{ width: '100%', height: '100vh' }} suppressHydrationWarning={true}>
           <FuecTemplate />
         </PDFViewer>
       }
