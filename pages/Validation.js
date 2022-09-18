cd7089a14e536a2e65576155c6d45d3727380e6c
marcelogodoybaquero <marcelo.godoy@mi.unc.edu.ar>
2022-09-18 18:59:30 -0300

    paginado full funcional

--- pages/Validation.js
+++ pages/Validation.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 import React, { useEffect, useState } from 'react'
-import validationHero from '../public/images/OnBoarding-3.svg'
+import validationHero from '../public/Images/OnBoarding-3.svg'
 import Image from 'next/image'
 const Nav = dynamic(() => import('../components/Nav'), { ssr: false })
 
