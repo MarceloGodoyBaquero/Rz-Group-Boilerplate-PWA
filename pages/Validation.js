667a6345090c26b43cc9bbfb502136437baa7c3f
marcelogodoybaquero <marcelo.godoy@mi.unc.edu.ar>
2022-09-19 12:38:54 -0300

    paginado full funcional

--- pages/Validation.js
+++ pages/Validation.js
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic'
 import React, { useEffect, useState } from 'react'
 import validationHero from '../public/Images/OnBoarding-3.svg'
 import Image from 'next/image'
+
 const Nav = dynamic(() => import('../components/Nav'), { ssr: false })
 
 export default function Validation () {
@@ -10,23 +11,24 @@ export default function Validation () {
     setMounted(true)
   }, [])
   return (
-  <div className={'md:shadow-2xl bg-[#F7F8FA] h-screen flex items-center flex-col'}>
-    <Nav location={'Validación Conductor'}/>
-    { mounted &&
-      <div className={'flex flex-col justify-center w-full'}>
-        <div className='flex flex-col justify-center items-center'>
+    <div className={'md:shadow-2xl bg-[#F7F8FA] h-screen flex items-center flex-col'}>
+      <Nav location={'Validación Conductor'}/>
+      {mounted &&
+        <div className={'flex flex-col justify-center w-full'}>
           <div className='flex flex-col justify-center items-center'>
-            <Image src={validationHero} alt="hero" className={'w-3/4'}/>
-          </div>
-          <div className='flex flex-col justify-center items-center text-center mt-20'>
-            <h2 className={'font-bold text-2xl ml-8 mr-8'}>Para utilizar el servicio necesitamos validar algunos documentos.</h2>
-            <p className='mt-8 ml-8 mr-8'>No te preocupes una vez los envies nuestro equipo lo revisara en un plazo menor a 48 horas.</p>
-          </div>
-          <div>
-
+            <div className='flex flex-col justify-center items-center'>
+              <Image src={validationHero} alt="hero" className={'w-3/4'}/>
+            </div>
+            <div className='flex flex-col justify-center items-center text-center mt-20'>
+              <h2 className={'font-bold text-2xl ml-8 mr-8'}>Para utilizar el servicio necesitamos validar algunos
+                documentos.</h2>
+              <p className='mt-8 ml-8 mr-8'>No te preocupes una vez los envies nuestro equipo lo revisara en un plazo
+                menor a 48 horas.</p>
+            </div>
+            <div>
+            </div>
           </div>
         </div>
-      </div>
-    }</div>
+      }</div>
   )
 }
