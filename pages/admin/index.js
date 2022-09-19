1c06e71a6dc2840bc8275f71a3f9003ed69d1c5c
marcelogodoybaquero <marcelo.godoy@mi.unc.edu.ar>
2022-09-19 15:15:04 -0300

    paginado full funcional

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import AdminLayout from '../../components/AdminLayout'
 import MobileLayout from '../../components/MobileLayout'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
 
 export default function index () {
   const [showing, setShowing] = useState(false)
-
+  const { user } = useSelector(state => state)
+  const router = useRouter()
   useEffect(() => {
     setShowing(true)
   }, [])
 
   if (!showing) return null
-
+  if (!user.role === 'admin') router.push('/')
   return (
     <MobileLayout>
       <AdminLayout/>
