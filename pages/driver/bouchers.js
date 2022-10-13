2eaebd1e4cb9bda2c17f939bc99611cc917f6e8c
MarceloGodoyBaquero <marcelo.godoy@mi.unc.edu.ar>
2022-10-13 17:52:23 -0300

    avances

---
+++ pages/driver/bouchers.js
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from 'react'
+import Nav from '../../components/Nav'
+import MobileLayout from '../../components/MobileLayout'
+import { useRouter } from 'next/router'
+import { useDispatch, useSelector } from 'react-redux'
+// eslint-disable-next-line
+import {getPaymentsByDriverId} from '../../Redux/Actions/paymentActions'
+import ClientBoucherCard from '../../components/ClientBoucherCard'
+import * as PropTypes from 'prop-types'
+
+ClientBoucherCard.propTypes = {
+  estado: PropTypes.string,
+  id: PropTypes.number
+}
+export default function travels ({ data }) {
+  const [tab, setTab] = useState(1)
+  // eslint-disable-next-line
+  const [subTab, setSubTab] = useState(1)
+  const router = useRouter()
+  const dispatch = useDispatch()
+  const { payments, user } = useSelector(state => state)
+
+  useEffect(() => {
+    dispatch(getPaymentsByDriverId(user.id))
+  }, [])
+  return (
+    <MobileLayout>
+      <div className={'md:shadow-2xl bg-[#F7F8FA] h-screen flex items-center flex-col'}>
+        <Nav location={'Mis Vouchers'}/>
+        {
+          payments && !payments.data.msg === 'not services found'
+            ? <div className={'flex flex-col justify-center w-full h-screen items-center'}>
+              <div className={'flex flex-col justify-center items-center'}>
+                <h1 className={'text-2xl font-bold'}>No tienes servicios creados!</h1>
+                <button onClick={() => router.push('/FuecForm')}
+                        className={'w-full rounded-[25px] h-[50px] text-white bg-[#5B211F]'}>
+                  CREAR SERVICIO
+                </button>
+              </div>
+            </div>
+            : <div className={'flex flex-col justify-center w-full items-center'}>
+              <div className={'flex flex-col justify-center w-full items-center'}>
+                <div
+                  className={'mt-5 flex bg-white rounded-2xl flex-row justify-around w-5/6 h-[50px] items-center drop-shadow-xl'}>
+                  <button onClick={(e) => setTab(1)}
+                          className={tab === 1 ? 'bg-yellow-400 w-full rounded-l-2xl h-[50px] font-bold' : 'bg-white w-full rounded-l-2xl h-[50px]'}>
+                            Pendientes
+                  </button>
+                  <button onClick={(e) => setTab(2)}
+                          className={tab === 2 ? 'bg-green-400 w-full rounded-r-2xl h-[50px] font-bold' : 'w-full rounded-r-2xl h-[50px]'}>
+                            Finalizados
+                  </button>
+                </div>
+                <div className={'mt-1 mb-5 flex w-full flex-row items-center justify-center'}>
+                  {tab === 1 &&
+                    <div className={'flex flex-col justify-center w-full items-center'}>
+                      <div className={'mt-5 mb-5 flex w-full flex-row items-center justify-center'}>
+                        {
+                          subTab === 1 &&
+                          <div className={'flex flex-col justify-center w-full items-center'}>
+                            {
+                              payments && payments?.data?.filter(e => !e.isPaid).length === 0
+                                ? <div className={'flex flex-col justify-center w-full items-center'}>
+                                <h1 className={'text-2xl font-bold'}>No tienes pagos pendientes!</h1>
+                              </div>
+                                : <div className={'flex flex-col justify-evenly w-full items-center ml-3 mr-3 mb-5'}>
+                                  <div className='flex flex-col justify-evenly items-start w-full mt-3 ml-5'>
+                                    <div className='flex flex-row justify-center items-center'>
+                                      <h1 className='font-bold'>
+                                        Vouchers pendientes:
+                                        <span className='text-red-500 font-bold ml-3'>{
+                                          payments && payments?.data?.filter(e => e.paymentType === 'voucher' && !e.isPaid).length
+                                        }</span>
+                                      </h1>
+                                    </div>
+                                  <div className='flex flex-row justify-evenly items-center'>
+                                  <h1 className='font-bold'>
+                                        Montos pendientes: $ <span className='text-red-500 font-bold'>{
+                                          payments && payments?.data?.filter(e => e.paymentType === 'voucher' && !e.isPaid).reduce((a, b) => a + b.paymentAmount, 0)
+                                        }</span>
+                                      </h1>
+                                    </div>
+                                  </div>
+                              </div>
+                            }
+                            {payments && payments?.data?.filter(e => !e.isPaid)?.map((item, index) =>
+                              <ClientBoucherCard key={index} id={index} estado={item.isPaid} paymentAmount={item.paymentAmount} paymentType={item.paymentType} startDate={item.start_date} driver={item.driver}/>
+                            )}
+                          </div>
+                        }
+                      </div>
+                    </div>}
+                  {tab === 2 &&
+                    <div className={'mt-5 flex flex-col justify-center w-full items-center'}>
+                      {
+                        payments && payments?.data?.filter(e => e.isPaid).length === 0
+                          ? <div className={'flex flex-col justify-center w-full items-center'}>
+                          <h1 className={'text-2xl font-bold'}>No tienes pagos finalizados!</h1>
+                        </div>
+                          : <div className={'flex flex-col justify-evenly w-full items-center ml-3 mr-3 mb-5'}>
+                        <div className='flex flex-col justify-evenly items-start w-full mt-3 ml-5'>
+                          <div className='flex flex-row justify-center items-center'>
+                            <h1 className='font-bold'>
+                              Vouchers Pagados:
+                              <span className='text-green-500 font-bold ml-3'>{
+                                payments && payments?.data?.filter(e => e.paymentType === 'voucher' && e.isPaid).length
+                              }</span>
+                            </h1>
+                          </div>
+                        <div className='flex flex-row justify-evenly items-center'>
+                        <h1 className='font-bold'>
+                              Total Vouchers: $ <span className='text-green-500 font-bold'>{
+                                payments && payments?.data?.filter(e => e.paymentType === 'voucher' && e.isPaid).reduce((a, b) => a + b.paymentAmount, 0)
+                              }</span>
+                            </h1>
+                          </div>
+                          <div className='flex flex-row justify-evenly items-center'>
+                          <h1 className='font-bold'>
+                             Servicios con efectivo: <span className='text-green-500 font-bold'>{
+                                payments && payments?.data?.filter(e => e.paymentType === 'cash' && e.isPaid).length
+                             }</span>
+                          </h1>
+                          </div>
+                            <div className='flex flex-row justify-evenly items-center'>
+                            <h1 className='font-bold'>
+                              Total efectivo: $ <span className='text-green-500 font-bold'>{
+                                payments && payments?.data?.filter(e => e.paymentType === 'cash' && e.isPaid).reduce((a, b) => a + b.paymentAmount, 0)
+                              }</span>
+                            </h1>
+                            </div>
+                        </div>
+                    </div>
+                      }
+                      {payments && payments?.data?.filter(e => e.isPaid)?.map((item, index) =>
+                              <ClientBoucherCard key={index} id={index} estado={item.isPaid} paymentAmount={item.paymentAmount} paymentType={item.paymentType} startDate={item.start_date} driver={item.driver}/>
+                      )}
+                    </div>}
+                </div>
+              </div>
+            </div>
+        }
+      </div>
+    </MobileLayout>
+  )
+}
---
+++ pages/driver/bouchers/[id].js
@@ -0,0 +1,123 @@
+import React from 'react'
+import MobileLayout from '../../../components/MobileLayout'
+import Nav from '../../../components/Nav'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { useSelector } from 'react-redux'
+
+export default function users (data) {
+  console.log(data)
+  const { user } = useSelector(state => state)
+  const router = useRouter()
+  const { id } = router.query
+
+  const confirmAction = () => {
+    if (user.roles === 'client') {
+      axios.post(`https://rz-group-backend.herokuapp.com/api/payment/confirmClient/${id}`)
+        .then(res => {
+          console.log(res)
+          console.log(user.roles)
+          return alert('pago confirmado')
+        }).catch(err => {
+          console.log(err)
+        }).then(res => {
+          router.reload()
+        })
+    } else if (user.roles === 'driver') {
+      return axios.post(`https://rz-group-backend.herokuapp.com/api/payment/confirmDriver/${id}`)
+        .then(res => {
+          console.log(res)
+          console.log(res.data)
+        }).catch(err => {
+          console.log(err)
+        })
+    }
+  }
+  return (
+    <MobileLayout>
+      <div className={'md:shadow-2xl bg-[#F7F8FA] h-screen flex items-center flex-col'}>
+        <Nav location={'Detalles del Pago'}/>
+        <div className={'mt-5 p-5 bg-white w-5/6 drop-shadow-2xl rounded-xl flex flex-col justify-evenly'}>
+          {
+            data.data.isPaid ? <h1 className={'text-center text-green-500 font-bold'}>SERVICIO PAGADO</h1> : <h1 className={'text-center font-bold text-orange-500'}>SERVICIO PENDIENTE DE PAGO</h1>
+          }
+          <hr/>
+          {
+            data.data.driver_confirm ? <h3 className={'text-center text-[14px] text-green-500 font-bold'}>CONFIRMADO POR CONDUCTOR</h3> : <h3 className={'text-center font-bold text-[14px] text-orange-500'}>CONDUCTOR CONFIRMACIÓN PENDIENTE </h3>
+          }
+          <hr/>
+          {
+            data.data.client_confirm ? <h3 className={'text-center text-[14px] text-green-500 font-bold'}>CONFIRMADO POR CLIENTE</h3> : <h3 className={'text-center font-bold text-[14px] text-orange-500'}>CLIENTE CONFIRMACIÓN PENDIENTE</h3>
+          }
+        </div>
+        <div className={'mt-5 p-5 bg-white w-5/6 drop-shadow-2xl rounded-xl flex flex-col justify-evenly'}>
+          <h1 className={'text-center font-bold'}>CLIENTE</h1>
+          <h1>Nombre: {data.data.client.firstName} {data.data.client.lastName}</h1>
+          <h1>Id: {data.data.client.idNumber}</h1>
+          <h1>Tel: {data.data.client.phoneNumber}</h1>
+          <h1>Email: {data.data.client.email}</h1>
+          <hr className={'mt-2'}/>
+          <label className={'text-center mt-2'}>
+            Firma:
+            <img src={data.data.client_signature} alt={'cliente-firma'}/>
+          </label>
+        </div>
+        {
+          data.data.client.companyAllied
+            ? <div className={'mt-5 p-5 bg-white w-5/6 drop-shadow-2xl rounded-xl flex flex-col justify-evenly'}>
+              <h1 className={'text-center font-bold'}>EMPRESA ALIADA</h1>
+              <h1>Nombre: {data.data.client.companyAllied.name}</h1>
+              <h1>{data.data.client.companyAllied.Id_type}: {data.data.client.companyAllied.Id_number}</h1>
+              <h1>Tel: {data.data.client.companyAllied.phone}</h1>
+              <h1>Email: {data.data.client.companyAllied.email}</h1>
+              <h1>Dirección: {data.data.client.companyAllied.address}</h1>
+            </div>
+            : null
+        }
+        <div className={'mt-5 p-5 bg-white w-5/6 drop-shadow-2xl rounded-xl flex flex-col justify-evenly'}>
+          <h1 className={'text-center font-bold'}>SERVICIO</h1>
+          <h1>Precio: ${data.data.paymentAmount}</h1>
+          <h1>Forma de pago: {data.data.paymentType}</h1>
+          <hr/>
+          <h1>Cantidad de vehículos: {data.data.number_vehicles}</h1>
+          <h1>Tipo: {data.data.serviceType}</h1>
+          <h1>Categoría: {data.data.category}</h1>
+          <hr/>
+          <h1>Inicio: {data.data.start_date.slice(0, 10)}</h1>
+          <h1>Final: {data.data.end_date.slice(0, 10)}</h1>
+          <h1>Origen: {data.data.from}</h1>
+          <h1>Destino: {data.data.to}</h1>
+          <hr/>
+          <h1>Descipción servicio: {data.data.description}</h1>
+        </div>
+        <div className={'mt-5 p-5 bg-white w-5/6 drop-shadow-2xl rounded-xl flex flex-col justify-evenly'}>
+          <h1 className={'text-center font-bold'}>CONDUCTOR</h1>
+          <h1>Nombre: {data.data.driver.firstName} {data.data.driver.lastName}</h1>
+          <h1>Id: {data.data.driver.idNumber}</h1>
+          <h1>Licencia: {data.data.driver.nro_license}</h1>
+          <h1>Email: {data.data.driver.email}</h1>
+          <hr/>
+          <h1>Descripción extras: {data.data.payment_description}</h1>
+        </div>
+        <div className={'flex flex-col w-full items-center'}>
+          <button onClick={() => confirmAction()}
+                  className={'bg-blue-400 w-5/6 rounded-xl mt-5 mb-5 h-[50px] font-bold'}>
+            CONFIRMAR PAGO
+          </button>
+        </div>
+      </div>
+    </MobileLayout>
+  )
+}
+
+export async function getServerSideProps (context) {
+  const { id } = context.query
+  const res = await fetch(`https://rz-group-backend.herokuapp.com/api/payment/${id}`)
+  const data = await res.json()
+  console.log(data)
+  return {
+    props: {
+      data
+    }
+  }
+}
