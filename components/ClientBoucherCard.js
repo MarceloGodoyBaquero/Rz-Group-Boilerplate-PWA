e6261b9eacbf7dc032eb88ca8e6c7568299d1499
Tiago Cornalo <cornalotiago@gmail.com>
2022-10-13 20:37:43 -0300

    hot fix

--- components/ClientBoucherCard.js
+++ components/ClientBoucherCard.js
@@ -6,7 +6,7 @@ export default function ClientBoucherCard ({ estado, id, paymentAmount, paymentT
   const router = useRouter()
   return (
     <div onClick={() => router.push(`/client/bouchers/${id}`)} className={'bg-white m-2 flex-row flex justify-evenly h-[100px] items-center w-full rounded'}>
-      <div className={`flex items-center justify-center w-[100px] h-[100px] ${paymentType === 'voucher' ? 'border-green-500 border-4' : 'border-red-500'} rounded-full`}>
+      <div className={`flex items-center justify-center w-[100px] h-[100px] ${paymentType === 'voucher' ? 'border-green-500 border-4' : 'border-red-500 border-4'} rounded-full`}>
         <h2 className={'font-bold text-black' }>
           {paymentType === 'cash' ? 'Efectivo' : 'Voucher'}
         </h2>
